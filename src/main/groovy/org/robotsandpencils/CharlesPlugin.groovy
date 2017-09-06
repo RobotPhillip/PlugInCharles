bcdbb6133e17ea9fae1c50e57e5894afd572aa8e
Phillip Wray <phillip.wray@robotsandpencils.com>
2017-09-06 15:58:47 -0600

    Remove build files, commit plugin code changes

--- src/main/groovy/org/robotsandpencils/CharlesPlugin.groovy
+++ src/main/groovy/org/robotsandpencils/CharlesPlugin.groovy
@@ -2,17 +2,18 @@ package org.robotsandpencils
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
-import org.robotsandpencils.CharlesManifest
+import org.gradle.api.tasks.Copy
 
 class CharlesPlugin implements Plugin<Project> {
 
     void apply(Project project) {
-        // Add the 'greeting' extension object
-        def extension = project.extensions.create('charlesManifest', CharlesPluginExtension, project)
-        // Add a task that uses the configuration
-        project.task('initializeCharles', type:CharlesToFileTask) {
-            message = extension.messageProvider
-            outputFiles = extension.outputFiles
+        def someTask = project.tasks.create('copyIml', Copy) {
+            from project.zipTree('/Users/pwray/robots/plugins/repo/org/gradle/charlesPlugin/1.0/charlesPlugin-1.0.jar')
+            include '**/charles.iml'
+            into 'charlesLib'
         }
+        someTask.dependsOn "consumeCharles"
+        someTask.group = "Charles to File"
     }
+
 }
--- src/main/groovy/org/robotsandpencils/CharlesToFileTask.groovy
+++ src/main/groovy/org/robotsandpencils/CharlesToFileTask.groovy
@@ -5,6 +5,7 @@ import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.util.GFileUtils
 
 class CharlesToFileTask extends DefaultTask {
 
@@ -43,9 +44,74 @@ class CharlesToFileTask extends DefaultTask {
     @TaskAction
     def configureManifest() {
         getOutputFiles().each {
-            logger.quiet "Writing port to manifiest"
+            logger.quiet "Writing port to manifest"
 //            it.text = getMessage()
-            it.append(getMessage()+"\n")
+            it.append(getMessage() + "\n")
         }
+
+        new File("charlesLib/src/main/res/xml").mkdirs()
+
+        def gitIgnoreFile = new File('charlesLib', '.gitignore')
+        gitIgnoreFile.text = ''
+        gitIgnoreFile << '''/build
+'''
+
+        def buildGradleFile = new File('charlesLib', 'build.gradle')
+        buildGradleFile.text = ''
+        buildGradleFile << '''apply plugin: \'com.android.library\'
+android {
+    compileSdkVersion 26
+    buildTypes {
+        debug {}
+        releaseApp {}
+        releaseSdk {}
     }
+    defaultConfig {
+        minSdkVersion 14
+    }
+}
+'''
+
+        def androidManifestFile = new File('charlesLib/src/main', 'AndroidManifest.xml')
+        androidManifestFile.text = ''
+        androidManifestFile << '''<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+          package="com.robotandpencils.charleslib">
+    <application
+        android:networkSecurityConfig="@xml/network_security_config">
+    </application>
+</manifest>
+'''
+
+        def networkSecurityConfigFile = new File('charlesLib/src/main/res/xml', 'network_security_config.xml')
+        networkSecurityConfigFile.text = ''
+        networkSecurityConfigFile << '''<?xml version="1.0" encoding="utf-8"?>
+<network-security-config>
+    <debug-overrides>
+        <trust-anchors>
+            <!-- Trust user added CAs while debuggable only -->
+            <certificates src="user" />
+        </trust-anchors>
+    </debug-overrides>
+</network-security-config>
+'''
+
+        def settingsGradleFile = new File('settings.gradle')
+        if (!settingsGradleFile.getText('UTF-8').contains('charlesLib')) {
+            settingsGradleFile << ", ':charlesLib'"
+        }
+
+        def appBuildGradleFile = new File('app/build.gradle')
+        if (!GFileUtils.readFile(appBuildGradleFile).contains('charlesLib')) {
+            new File('app/tmp.build.gradle').withWriter { w ->
+                new File('app/build.gradle').eachLine { line ->
+                    w << line.replaceAll('dependencies \\{', 'dependencies \\{\n    implementation project(\':charlesLib\')') + System.getProperty("line.separator")
+                }
+            }
+            new File('app/build.gradle').renameTo('app/old.build.gradle')
+            new File('app/tmp.build.gradle').renameTo('app/build.gradle')
+        }
+
+    }
+
 }
